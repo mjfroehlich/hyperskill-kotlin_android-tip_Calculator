ee2c6be42312fdbb463beef7ed81e07a5367c9de
Martin Froehlich <4023770+mjfroehlich@users.noreply.github.com>
2023-10-08 20:52:54 +0200

    Stage 3 quick-and-dirty

--- Tip Calculator/task/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
+++ Tip Calculator/task/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
@@ -13,27 +13,31 @@ class MainActivity : AppCompatActivity() {
     val textBillAmount: TextView by lazy { findViewById(R.id.bill_value_tv) }
     val tipPercentTv: TextView by lazy { findViewById(R.id.tip_percent_tv) }
     val tipPercentSeekBar: SeekBar by lazy { findViewById(R.id.seek_bar) }
+    val tipAmountTv: TextView by lazy { findViewById(R.id.tip_amount_tv) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         editText.addTextChangedListener(object:TextWatcher {
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            override fun onTextChanged(seq: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 val tipPercent = tipPercentSeekBar.progress
-                val amountSt = p0.toString()
+                val amountSt = seq.toString()
                 if (amountSt.isEmpty()) {
                     textBillAmount.text = null
                     tipPercentTv.text = null
+                    tipAmountTv.text = null
                     return
                 }
                 val amount = amountSt.toDouble()
                 if (amount > 0) {
                     textBillAmount.text = String.format("Bill Value: $%.2f", amount)
                     tipPercentTv.text = String.format("Tip: %d%%", tipPercent)
+                    tipAmountTv.text = String.format("Tip Amount: $%.2f", amount * tipPercent / 100)
                 } else {
                     textBillAmount.text = null
                     tipPercentTv.text = null
+                    tipAmountTv.text = null
                 }
             }
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -50,16 +54,20 @@ class MainActivity : AppCompatActivity() {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, p2: Boolean) {
                 if (editText.text.isEmpty()) {
                     tipPercentTv.text = null
+                    tipAmountTv.text = null
                     return
                 }
-                val amountSt = editText.text.toString()
-                if (amountSt.isEmpty()) {
+                val billValueText = editText.text.toString()
+                if (billValueText.isEmpty()) {
                     tipPercentTv.text = null
+                    tipAmountTv.text = null
                     return
                 }
-                val amount = amountSt.toDouble()
+                val amount = billValueText.toDouble()
+                val tipPercent = progress
                 if (amount > 0) {
-                    tipPercentTv.text = String.format("Tip: %d%%", progress)
+                    tipPercentTv.text = String.format("Tip: %d%%", tipPercent)
+                    tipAmountTv.text = String.format("Tip Amount: $%.2f", amount * tipPercent / 100)
                 }
             }
 
